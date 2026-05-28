d4e11b4c9dfb7389be2a55713de5cd64f0312dbd
Sriesa Chatterjee <sri.esa09@gmail.com>
2026-05-28 13:42:11 +0000

    datatypes in js

---
+++ 00_basics/01_datatype.js
@@ -0,0 +1,16 @@
+"use strict";//treat all the code as newer version
+
+//alert(3+3)//ERROR we're using node.js
+
+console.log(3
+    +3
+)//avoid these, make code readable
+
+//number =>2^53
+// bigint
+// null=> standalone value
+// undefined=> haven't been assigned with anything
+//symbol=> sets uniqueness
+console.log(typeof "dog")
+console.log(typeof undefined)
+console.log(typeof null)//doesn't have unique type
